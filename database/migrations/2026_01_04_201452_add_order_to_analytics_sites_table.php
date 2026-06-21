71beab287d528c77308532c5350fd3c611d2b3cb
xattir1 <xattier1@gmail.com>
2026-06-21 15:44:43 +0300

    remove after(user_id)

--- database/migrations/2026_01_04_201452_add_order_to_analytics_sites_table.php
+++ database/migrations/2026_01_04_201452_add_order_to_analytics_sites_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('analytics_sites', function (Blueprint $table) {
-            $table->integer('order')->default(0)->after('user_id');
+            $table->integer('order')->default(0);
             $table->index('order');
         });
     }
